97cfb61613f2a73bd019ca4a3f1db629cec0af2c
Gabor Fekete <gabor.fekete@tekla.com>
2014-07-18 10:49:07 +0300

    Experiment with making tuple from a function's parameter list.

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -1,14 +1,36 @@
-﻿#include "header1.hpp"
-
-#include <string>
+﻿#include <string>
 #include <iostream>
+#include <functional>
+#include <utility>
+#include <tuple>
+#include <type_traits>
+
+//template <>
+//struct MakeTuple_s
+//{
+//
+//};
+//
+//template <typename TFuncRet, typename TFuncArg1, typename ...TFuncArgRest>
+//auto ExtractFunctionParameterList(TFuncRet (*pFunction) (TFuncArg1, TFuncArgRest...))
+//-> MakeTuple_s<TFuncArg1, TFuncArgRest...>::TupleType
+//{
+//    return MakeTuple_s<TFuncArg1, TFuncArgRest...>::TupleType{};
+//}
+
+
+template <typename TFuncRet, typename ...TFuncArgs>
+std::tuple<typename std::remove_reference<TFuncArgs>::type...>
+MakeTupleFromParameterList(TFuncRet (*pFunction) (TFuncArgs...))
+{
+    return std::tuple<typename std::remove_reference<TFuncArgs>::type...>();
+}
+
+void TestFunction(int, const std::string &) {}
 
 int main()
 {
-    auto DoubleVector = std::vector<double>{};
-    auto pCharPointer = "Hello";
-
-    RemoteFunction(DoubleVector, pCharPointer);
-
+    auto ParameterList = MakeTupleFromParameterList(TestFunction);
+    std::cout << typeid(ParameterList).name() << std::endl;
     return 0;
 }
