305f55524b4d3559757a6040bdb1d0f809a4ca75
Gabor Fekete <gabor.fekete@tekla.com>
2014-09-22 08:32:32 +0300

    Using unique_ptr with malloc and free.

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -63,90 +63,36 @@
 //    return static_cast<int>(a.arr[40]);
 //}
 
-#include <iostream>
-#include <fstream>
-#include <string>
 
-std::string GetTableName(const std::string &Line)
-{
-    const auto TablePos = Line.find("Table ");
-    if(TablePos == std::string::npos)
-    {
-        return "";
-    }
-    const auto NameStartPos = TablePos + 6;
-    const auto NameEndPos = Line.find(' ', NameStartPos);
-    return Line.substr(NameStartPos, NameEndPos - NameStartPos);
-}
-
-std::string DumpTableToFile(std::ifstream &InStream, const std::string &TableName)
-{
-    std::cout << "Dumping " << TableName << std::endl;
-
-    const auto OutFile = std::string{"C:/Temp/scratchpad_dump/scratchpad.dump."} + TableName;
-    auto OutStream = std::ofstream{OutFile, std::ios::app};
-    if(!OutStream.is_open())
-    {
-        std::cout << "Cannot open output file:" << OutFile << std::endl;
-        return false;
-    }
-
-    OutStream << "Table: " << TableName << "\n";
-
-    auto IsTableFound = false;
-    auto Line = std::string{};
-    do {
-        std::getline(InStream, Line);
-        if(Line.find("Table ") != std::string::npos)
-        {
-            IsTableFound = true;
-        }
-        else
-        {
-            OutStream << Line << "\n";
-        }
-    } while(InStream.good() && !IsTableFound);
-
-    return Line;
-}
+#include <memory>
 
 int main()
 {
-    const auto InFile = std::string{"C:/Models/TestModel/model.dmp"};
-    auto InStream = std::ifstream{InFile};
-    if(!InStream.is_open())
-    {
-        std::cout << "Cannot open file" << std::endl;
-        return 0;
-    }
-    auto Line = std::string{};
-
-    auto IsTableFound = false;
-    std::getline(InStream, Line);
-    do {
-        const auto &TableName = GetTableName(Line);
-        if(!TableName.empty())
-        {
-            Line = DumpTableToFile(InStream, TableName);
-        }
-        else
-        {
-            std::getline(InStream, Line);
-        }
-    } while(InStream.good());
-
+    auto Data =
+        std::unique_ptr<double, void(*)(void*)>{
+            reinterpret_cast<double*>(malloc(sizeof(double) * 50)),
+            free };
     return 0;
 }
 
+
 //#include <memory>
 //#include <iostream>
+//
 //void MyFree(void *p)
 //{
 //    free(p);
 //    std::cout << "MyFree()" << std::endl;
 //}
+//
+//template <typename T>
+//struct TypeDisplayer;
 //int main()
 //{
-//    std::unique_ptr<void, decltype(MyFree)*> m{(void*)malloc(1000), MyFree};
+//    // TypeDisplayer<decltype(MyFree)*> MyFreeType;
+//    // main.cpp(77): error C2079: 'MyFreeType' uses undefined struct 'TypeDisplayer<void (__cdecl *)(void *)>'
+//
+//    auto m1 = std::unique_ptr<void, decltype(MyFree)*>{ malloc(1000), MyFree };
+//    auto m2 = std::unique_ptr<void, decltype(free)*>{ malloc(1000), free };
 //    return 0;
 //}
